6c40e12febaaa0d6ec5e67af5003bc812c416047
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2023-11-10 16:54:53 +0545

    Add files via upload

--- Problem6.c
+++ Problem6.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
-    int n =100;
-    long int sum_square = 0,square_sum =0;
+    int n=1,num =2; //setting 2 as the 1st term
+    while (n !=10001)
+    {
+        num++;  //going to another number for checking
+        int isPrime = 1;
+        for (int i = 2;i <=sqrt(num); i++)  //checking
+        {
 
-    sum_square = n*(n+1)*(2*n+1)/6;
-    square_sum = n*(n+1)/2;
+            if (num%i == 0){
+                isPrime = 0;
+                break;
+            }
+        }
 
-    square_sum = square_sum*square_sum;
-    printf("%ld",square_sum - sum_square);
+        if (isPrime){   //if true then we found another term so increase the nth term by 1
+            n++;
+        }
+
+
+
+    }
+    printf("%d\n",num);
 
 
 
