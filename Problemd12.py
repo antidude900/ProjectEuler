6dcc3d179cfd7dd1936a3d26709a00957a09c489
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2023-11-25 17:22:16 +0545

    Added

--- Problemd12.py
+++ Problemd12.py
@@ -1,28 +1,27 @@
-def tau(num) :
+#see https://www.youtube.com/watch?v=9WX8_qluq7I&t=491s upto 2:46 to understand algorithm
+def no_of_factors(num) :
 
     n = num;
     i = 2;
     p = 1
 
-    while (i <= n) :
-        c = 1
+    while (i <= n) :  #same prime factor process as in question 3
+        c = 1         #saving the no of times the number divides it(set it 1 at first for the +1 we have to do at the end)
         while (n % i == 0):
             n//= i
             c+=1
 
         i+=1
-        p*= c
+        p*= c    
 
     return p
 
-def solution(x):
-    n = 1
-    d = 1
 
-    while (tau(d) <= x) :
-        n+=1
-        d+= n
-   
-    return d
+n = 1    #nth term
+num = 1    #the traingular number
 
-print(solution(500))
+while (no_of_factors(num) <= 500) : #saying over 500 dividers so <=
+    n+=1    #as next term,increasing the nth term
+    num+= n   #the traingular number increases by nth term(1+2+3+4+5+6+7+8 = 28 as said in the question)
+
+print(num)
