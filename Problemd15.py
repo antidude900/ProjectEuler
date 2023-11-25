f2900e52e2c3b79a0b0a26a96bf36689e42358a6
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2023-11-25 21:05:38 +0545

    Added

--- Problemd15.py
+++ Problemd15.py
@@ -1,10 +1,20 @@
-import numpy as np
+#combination formula(to understand more go https://www.youtube.com/watch?v=ivYXjESJ_zE)
+from math import factorial
 
-grid = np.full((21,21), 1,dtype=np.int64)
+total = factorial(40)//(factorial(20)*factorial(40-20)) 
+print(total)
 
 
-for i in range(1,21):
-	for j in range(1,21):
-		grid[i][j] = grid[i-1][j]+grid[i][j-1]
+#but what if not a square grid?
+#then use this algorithm below(https://www.youtube.com/watch?v=ZEYG4tpI4U4)
 
-print(grid[20][20])
+#import numpy as np
+#
+#grid = np.full((21,21), 1,dtype=np.int64) #need to make it one size for both rows and column
+#
+#
+#for i in range(1,21):
+#	for j in range(1,21):
+#		grid[i][j] = grid[i-1][j]+grid[i][j-1]
+#
+#print(grid[20][20])
