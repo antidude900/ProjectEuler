c55562807ac0266e9b2cdcea0920b34fedab91c5
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2023-11-25 17:28:31 +0545

    Added

--- Problem3.py
+++ Problem3.py
@@ -1,10 +1,11 @@
-num,i = 600851475143,2;	# starting with number 2 to check all prime factors of num
+#alogirthm to find prime factors of a number
 
-while (i<=num):	#we will have factors until the number we divide with exceeds the number itself
+num,factor = 600851475143,2;	# starting with number 2 to check all prime factors of num
 
-	if (num%i == 0):	#checking if it is a factor
-		num /=i	#factor decrease the num by factor so the num goes on shrinking until the numb is not divisible by the factor
-
+while (num>=factor):	#we will have factors until the factor exceeds the number its dividing
+	if (num%factor == 0):	#if it is a factor
+		num /=factor	#factor decrease the num by division so the num goes on shrinking until the numb is not divisible by the factor
 	else:
-		i+=1	#if not a factor go to another number for checking
-print(i);
+		factor+=1	#if not a factor go to another number for checking
+	
+print(factor);
