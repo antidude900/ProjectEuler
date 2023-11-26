8e65af963087d28cd72fe515aa5e74baa7d70957
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2023-11-26 09:45:16 +0545

    Update Problemd16.py

--- Problemd16.py
+++ Problemd16.py
@@ -1,11 +1,9 @@
+sum=0
 
-product,sum=1,0
+n=pow(2,1000) #for 2 to the power 1000
 
-for i in range(1000):
-	product *= 2
-
-num = str(product)
+num = str(n) #so that we can iterate throught the number
 
 for i in range(len(num)):
-	sum+=int(num[i])
+	sum+=int(num[i]) #adding all the digits
 print(sum)
