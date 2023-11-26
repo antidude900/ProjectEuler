0c581cb6a9f96c6e0c59da0af4a6357e2e5b4d0b
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2023-11-26 09:57:22 +0545

    Update Problemd20.py

--- Problemd20.py
+++ Problemd20.py
@@ -2,12 +2,12 @@ product = 1
 sum = 0
 num = 100
 
-while (num!=1):
+while (num!=1):#finding 100 factorial
 	product *= num
 	num-=1
 
-tmp = str(product)
+tmp = str(product)#changing into string to iterate
 
-for i in tmp:
+for i in tmp:#adding all the digits
 	sum+=int(i)
 print(sum)
