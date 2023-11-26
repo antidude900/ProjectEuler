7aef096a7dcddf34662d96f192f532c4365842b3
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2023-11-26 09:55:31 +0545

    Update Problemd19.py

--- Problemd19.py
+++ Problemd19.py
@@ -1,4 +1,4 @@
-#January, February, march, April, May, June, July, August, September, October, November,December
+#will explain this orally too
 months = [31,29,31,30,31,30,31,31,30,31,30,31]
 days = 2
 sundays = 0
