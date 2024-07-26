08d05ff49c7c0956b059b6bdd51e6f9246176221
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-07-27 00:27:08 +0545

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -1,7 +1,25 @@
-sum = 0
+#"" parts are remaning parts to be explained and will explain them after exam ends
+def general_sum(n,k):
+    p = n//k ""
+    return k*p*(p+1)//2 ""
+"""
+    if done /2; though only an integer value,it will give .0 value and changing it into int by int() will make some precision issue and might not solve some edge cases 
+    (for me, case 2 and 3 didn't work when doing from hackerrank: https://www.hackerrank.com/contests/projecteuler/challenges/euler001/problem)
+    so best way to change to floating point to integer is by // and not int()
+"""
 
-for i in range(1000):	# checking all numbers less than 1000
-	if (i%3==0 or i% 5 == 0):	# if divisible by either 3 or 5, then include it in the sum
-		sum += i
 
-print(sum)
+def sum_multiple(n):
+    #sum of
+
+    multiple_3 = general_sum(n,3)
+    multiple_5 =general_sum(n,5)
+    multiple_15 =general_sum(n,15)
+    
+    multiple_3_or_5= multiple_3+multiple_5-multiple_15 ""
+    return multiple_3_or_5
+    
+
+n= int((input("Enter n:")))
+print(sum_multiple(n-1))    
+    
