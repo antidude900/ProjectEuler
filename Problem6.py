1200862e48c64d495cc40b02a858397ec9e69f3f
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-08-17 22:42:26 +0545

    Update Problem6.py

--- Problem6.py
+++ Problem6.py
@@ -1,8 +1,16 @@
-n = 100
+"""
+Find the absolute difference between the sum of the squares of the first n natural numbers and the square of the sum.
+"""
 
-sum_square = n*(n+1)*(2*n+1)/6; #formula for sum we read in sequence and series
-								#fun fact: I came up with this idea instead of looping in first thought!
-square_sum = n*(n+1)/2;
 
-square_sum = square_sum*square_sum;
-print(square_sum - sum_square);
+def abs_diff(n):
+
+    sum_square = n*(n+1)*(2*n+1)//6;	#formula for sum we read in sequence and series
+                                		#fun fact: I came up with this idea instead of looping in first thought!
+    square_sum = n*(n+1)//2;
+
+    square_sum = square_sum*square_sum;
+    return square_sum - sum_square
+
+n = int(input("Enter n:"))
+print(abs_diff(n))
