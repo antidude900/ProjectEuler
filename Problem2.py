582186b5dad810b4517a7630f911b96259a3159b
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-08-17 22:17:59 +0545

    Update Problem2.py

--- Problem2.py
+++ Problem2.py
@@ -1,13 +1,21 @@
-first,second,sum = 1,2,0 #initializing the first and second term of the sequence as 1 and 2
+"""
+By considering the terms in the Fibonacci sequence whose values do not exceed , find the sum of the even-valued terms.
+https://www.hackerrank.com/contests/projecteuler/challenges/euler002/problem?isFullScreen=true
+"""
 
-while (second <= 4000000): #checking all fibo numbers until number exceeds 4 million
+def fibo_sum(n):
+    first,second,sum = 1,2,0 #initializing the first and second term of the sequence as 1 and 2
 
-	if (second % 2 == 0):	#// if even include it in the sum
-		sum += second
+    while (second <= n): #checking all fibo numbers until number exceeds 4 million
+    
+        if (second % 2 == 0):    #// if even include it in the sum
+            sum += second
+    
+        temp = second #set the next two consecutive terms of the fibo seq
+        second = first + second
+        first = temp
+    
+    return sum
 
-	temp = second #set the next two consecutive terms of the fibo seq
-	second = first + second
-	first = temp
-
-print(sum)
-
+n = int(input("Enter n:"))
+print(fibo_sum(n))
