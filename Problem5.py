55d56844c710d7901dff0f8a72f6606e1861d068
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-08-17 22:29:51 +0545

    Update Problem5.py

--- Problem5.py
+++ Problem5.py
@@ -1,3 +1,8 @@
+"""
+What is the smallest positive number that is evenly divisible(divisible with no remainder) by all of the numbers from  1 to n?
+https://www.hackerrank.com/contests/projecteuler/challenges/euler005/problem?isFullScreen=true
+"""
+
 '''
 The smallest multiple between an interval is the lcm of the interval so we find the lcm of the interval
 
@@ -8,21 +13,22 @@ also to find lcm we know the formula lcm(a,b) = a*b/gcd(a,b)
 so in the first iteration taking lcm of LCM ie 1 and i ie 2, we do lcm = 1*2/gcd(1,2) giving lcm of 1 and 2 i.e 2
 and then in second iteration taking lcm of LCM i.e 2 and i i.e 3, we do lcm = 2*3/gcd(2,3)
 giving us the lcm of lcm(1,2) with 3 and then that with 4 and so on till 20
-
 '''
-
 
 from math import gcd
-LCM =1
+def divisible_by_all(n):
+    LCM =1
+    
+    for i in range (2,n+1):
+        LCM = int((LCM*i) / gcd(LCM,i))
+    
+    return LCM
 
-for i in range (2,21):
-	LCM = int((LCM*i) / gcd(LCM,i))
-
-print(LCM)
+n = int(input("Enter n:"))
+print(divisible_by_all(n))
 
 '''
-If wont let use other library such as math make your own gcd function like this:
-(but theres a high chance they will let us use math function for functions like sqrt)
+If want to make your own, gcd function then do this way:
 
 def gcd(x, y):
 
