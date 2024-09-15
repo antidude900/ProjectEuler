57750efce9f8f17fcc2bc911e638bba4070c4c74
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-09-15 22:59:26 +0545

    Update Problem9.py

--- Problem9.py
+++ Problem9.py
@@ -1,12 +1,12 @@
 """
 https://www.hackerrank.com/contests/projecteuler/challenges/euler009/problem
-https://martin-ueding.de/posts/project-euler-solution-9-special-pythagorean-triplet/
 """
 
 def pytho_triplet(n):
     result=-1
-    for a in range(1, n//3):
-        for b in range(a, n//2 - a//2):
+    for a in range(1, n//3): #any number can't be bigger than one third of the number as we have to express the number in sum of three
+        for b in range(a, ((n-a)//2)+1)): #We know that c>b>a, and c=n−a−b. We can combine these into n−a−b>b, or n−a>2b or (n-a)/2>b
+                                
             c = n - a - b
             if a**2 + b**2 == c**2:
                 result = max(result,a*b*c)
