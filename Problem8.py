4d033d6d7f4f14311c5e2b0dc78ee5379ec03393
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-09-15 14:03:32 +0545

    Update Problem8.py

--- Problem8.py
+++ Problem8.py
@@ -1,41 +1,32 @@
-num = '''73167176531330624919225119674426574742355349194934
-96983520312774506326239578318016984801869478851843
-85861560789112949495459501737958331952853208805511
-12540698747158523863050715693290963295227443043557
-66896648950445244523161731856403098711121722383113
-62229893423380308135336276614282806444486645238749
-30358907296290491560440772390713810515859307960866
-70172427121883998797908792274921901699720888093776
-65727333001053367881220235421809751254540594752243
-52584907711670556013604839586446706324415722155397
-53697817977846174064955149290862569321978468622482
-83972241375657056057490261407972968652414535100474
-82166370484403199890008895243450658541227588666881
-16427171479924442928230863465674813919123162824586
-17866458359124566529476545682848912883142607690042
-24219022671055626321111109370544217506941658960408
-07198403850962455444362981230987879927244284909188
-84580156166097919133875499200524063689912560717606
-05886116467109405077541002256983155200055935729725
-71636269561882670428252483600823257530420752963450'''.replace("\n","")
+"""
+Find the greatest product of k consecutive digits in the n digit number.
+https://www.hackerrank.com/contests/projecteuler/challenges/euler008/problem
+"""
 
-#taking the number as string as too big and formatting such that
-#the spaces we automatically got why pasting is number are removed
+def greatest_product(num,size,adj):
+    g_p=0	#initializing the greatest product to 0 
+    
+    for i in range(size - adj): 
+        """looping till we get to the final index minus number of adjacent number 
+	so that we wont exceed the index of the list while running the second  loop """
+	    
+        p = 1	#initilaizing the initial product to 1 
 
-size = len(num)
-adj = 13
-p,g_p=1,1
+	"""The reason why p=1 but g_p=0 is that suppose we had g_p=1 and all the consecutive product gives 0 
+ 	i.e p=0 in all the consecutive products then the g_p will always stay 1 as g_p is greater than p in all case
+  	so though our greates_product should have been 0 it gives greatest proudct as 1. So to handle this edge case,
+   	we used g_p as 0 so that any product that will come at beginning with be the greatest product"""
+	
+    
+        for j in range(i,i + adj): #looping from a element to 12 more from it(1 +12=13)
+            p *= int(num[j]) #multiply all the 13 adjacent elements
+            
+        
+            if (p > g_p and j==i+adj-1): 
+                #if greater than the greatest product, it is the  greatest product
+                g_p = p
 
-for i in range(size - adj): 
-	#looping till we get to the final index minus number of adjacent numbers
-	#so that we wont exceed the index of the list while running the second loop 
-	p = 1
-
-	for j in range(i,i + adj): #looping from a element to 12 more from it(1+12=13)
-		p *= int(num[j]) #multiply all the 13 adjacent elements
-
-
-		if (p > g_p): #if greater than the greatest product, it is the greatest product
-			g_p = p
-
-print(g_p)
+    return g_p
+	
+num,n,k=int(input("Enter the number, its size and the lenght of consecutive numbers for product"))
+print(greatest_product(num,n,k))
