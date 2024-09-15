3c688c8790106248672937e9db33fefb4cc1cf6b
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-09-15 22:43:48 +0545

    Update Problem9.py

--- Problem9.py
+++ Problem9.py
@@ -1,19 +1,17 @@
-from math import sqrt
+"""
+https://www.hackerrank.com/contests/projecteuler/challenges/euler009/problem
+https://martin-ueding.de/posts/project-euler-solution-9-special-pythagorean-triplet/
+"""
 
-triplet = []
-isResult = False
+def pytho_triplet(n):
+    result=-1
+    for a in range(1, n//3):
+        for b in range(a, n//2 - a//2):
+            c = n - a - b
+            if a**2 + b**2 == c**2:
+                result = max(result,a*b*c)
+                
+    return result
 
-for i in range(1,1000): #taking random a and b for the triplet
-	for j in range(1,1000):
-
-		k = sqrt((i*i)+(j*j)) #making c for the triplet
-
-		if (i+j+k == 1000):	#checking if they fulfill condition a+b+c =1000
-			#if yes then stop looping as we got our triplet
-			isResult = True
-			product = i*j*k
-			break
-	if isResult:
-		break;
-
-print(product)
+n = int(input("Enter n"))
+print(pytho_triplet(n))
