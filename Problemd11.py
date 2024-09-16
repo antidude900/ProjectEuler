322061c224543709cf317378692562886c0424bf
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-09-17 00:27:00 +0545

    Update Problemd11.py

--- Problemd11.py
+++ Problemd11.py
@@ -1,72 +1,47 @@
-import numpy as np
-
-num = '''08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
-49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
-81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
-52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
-22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
-24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
-32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
-67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
-24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
-21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
-78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
-16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
-86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
-19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
-04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
-88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
-04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
-20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
-20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
-01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48'''.replace("\n"," ").split(" ")
+def product_of_four(lst): #for carrying out product part of the adjacent numbers
+    max_product = 0
+    for i in range(len(lst) - 3):
+        product = lst[i] * lst[i + 1] * lst[i + 2] * lst[i + 3]
+        if product > max_product:
+            max_product = product
+    return max_product
 
 
-
-lst =  np.zeros((20,20))	#making a array/list of 20*20 grid	
-index = 0			#for iterating through the um string
-
-for i in range(20):		#adding the numbers to the 20*20 grid
-	for j in range(20):
-
-		lst[i][j] = num[index]
-		index += 1
-
-adj = 4 #have to look all 4 adjacent values
-g = 0	#denotes greaters value
-
-for i in range(20):
-	for j in range(20):
-		row_true = (i+adj <= 20)	#checking if going 4 steps from the curret i(row) will be within the 20*20 grid
-		col_true = (j+adj <= 20)	#checking if going 4 steps from the curret j(col) will be within the 20*20 grid
-
-		p = 1	#resetting product as 1 
-		if (row_true): #if within the grid (for row), 
-			for k in range(i,adj):	#then can iterate through all the 4 adjacent number within the current row
-				p *= lst[i+k][j]#if product of the 4 adjacent,
-				g = max(g,p)	#is greater than all the previous products, then change the greates value
-
-		p = 1
-		if (col_true):	#if within the grid (for column),
-			for k in range(j,adj):	#then can iterate through all the 4 adjacent number within the current column
-				p *= lst[i][j+k]
-				g = max(g,p)	
-
-		p = 1
-		if (row_true and col_true):	#if within the grid(for diagonal forward),
-			for k in range(0, adj):	#then can iterate through all the 4 adjacent number within the current column
-				p *= lst[i+k][j+k]
-				g = max(g,p)
-
-		p = 1
-		if (i>=4 and col_true):	#if within the grid(for diagonal backward),
-			for k in range(0, adj):	#then can iterate through all the 4 adjacent number within the current column
-				p *= lst[i-k][j+k]
-				g = max(g,p)
+    
+def greatest_product(grid):
 
 
-print(g)
+    max_product = 0
+    n = len(grid)
+    
+    # Check horizontal and vertical products
+    for i in range(n):
+        # Horizontal
+        max_product = max(max_product, product_of_four(grid[i]))
+                                       #sending a row to find the greatest product of four adjacent numbers of the row
+                    
+        # Vertical
+        col = [grid[j][i] for j in range(n)] #making the column constant and row changing to find all the numbers of the row
+        max_product = max(max_product, product_of_four(col))
+                                       #sending a column to find the greatest product of four adjacent numbers of the column
 
+    # Check diagonal products
+    for i in range(n - 3):
+        for j in range(n - 3):
+            # Top-left to bottom-right
+            diag1 = [grid[i + k][j + k] for k in range(4)] #increasing both row and column linearly to get diagonal starting from (i,j) to (i+3,j+3)
+            max_product = max(max_product, product_of_four(diag1))
 
-	
+            # Bottom-left to top-right
+            diag2 = [grid[i + 3 - k][j + k] for k in range(4)] #here the point i won't be the starting point of the diagonal but the starting point of the square which extends horizontally towards right and vertically downwards making a 4*4 square. Then we take the diagonal of the square from bottom left to top-right
+            max_product = max(max_product, product_of_four(diag2))
 
+    return max_product
+    
+num=input("Enter the string of 400 numbers:")
+num = list(map(int, num))
+
+size = 20
+grid = [num[i * size:(i + 1) * size] for i in range(size)]
+	#slicing num from starting point i*size to (i+1)*size thus making a sublist of length "size"
+print(greatest_product(grid))
