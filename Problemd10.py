920f8aa93559b1dcf18b9459401c22b9bf77e7f4
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-09-22 17:02:59 +0545

    Update Problemd10.py

--- Problemd10.py
+++ Problemd10.py
@@ -1,4 +1,9 @@
 """
-Leave for now
+When doing this question, I got to a big problem. There were 7 test cases in total. Out of 7, 5 of them were solved but 2 of them exceeded time limit. 
+I tried optmizing the code as much as I can but still 6 and 7 never solved. Then seeing the discussion section, I found out the mistake.
 
+The problem was that for every test case I was making a sieve array from start. The efficient way was to find the maximum N among all the test cases
+and make a sieve array of that maximum. In this way, ewe can use the same sieve array for all test cases saving time.
+
+[To understand how the test cases are read, see the ReadMe of this repository]
 """
