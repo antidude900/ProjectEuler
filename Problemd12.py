0a58cdd7610c4da09bd5ac063323fc48ef178933
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-09-23 18:07:05 +0545

    Update Problemd12.py

--- Problemd12.py
+++ Problemd12.py
@@ -1,5 +1,15 @@
-#see https://www.youtube.com/watch?v=9WX8_qluq7I&t=491s upto 2:46 to understand algorithm
-def no_of_factors(n) :
+"""
+What is the value of the first triangle number to have over N divisors?
+https://www.hackerrank.com/contests/projecteuler/challenges/euler012/problem
+"""
+
+"""
+This solution doesn't solve the problem of repeititive process for every single test cases
+"""
+
+import math
+# Function to count the number of divisors
+def count_divisors(n):
 
     i = 2;
     p = 1
@@ -16,11 +26,18 @@ def no_of_factors(n) :
     return p
 
 
-n = 1    #nth term
-num = 1    #the traingular number
+def nth_divisible_triangular(threshold):
+    n = 1 
+    triangular_number = 0
+    while True:
+        triangular_number = n * (n + 1) // 2 #using the sum of natural numbers formulato take sum of n numbers
+        if count_divisors(triangular_number) > threshold:
+            return triangular_number
+        n += 1
+        
+if __name__ == '__main__':
+    t = int(input().strip())
 
-while (no_of_factors(num) <= 500) : #saying over 500 dividers so <=
-    n+=1    #as next term,increasing the nth term
-    num+= n   #the traingular number increases by nth term(1+2+3+4+5+6+7+8 = 28 as said in the question)
-
-print(num)
+    for t_itr in range(t):
+        n = int(input().strip())
+        print (nth_divisible_triangular(n))
