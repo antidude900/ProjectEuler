8dd679123921dffc846573cf2f093aeae13c6d01
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-09-23 18:25:03 +0545

    Create Problem10.py

---
+++ Problem10.py
@@ -0,0 +1,28 @@
+"""
+Find the sum of all the primes not greater than given N.
+https://www.hackerrank.com/contests/projecteuler/challenges/euler010/problem
+"""
+
+
+def prime_sieve(n):
+    if n<2:
+        return 0
+    
+    prime = [True] * (n+1) #as index starts from 0, we increase limit size by 1 (otherwise the index will be from 0 to limit-1 thus excluding limit)
+    sum = 0  
+
+    # Sieve of Eratosthenes
+    for i in range(2, n+1):
+        if prime[i]:  
+            sum+=i
+            for j in range(i * i, n+1, i):
+                prime[j] = False
+    return sum
+
+
+if __name__ == '__main__':
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+        print(prime_sieve(n))
