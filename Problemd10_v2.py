319069e6df4ec5769c67329ce7dd81dc8e2393d8
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-09-23 18:30:25 +0545

    Update and rename Problemd10.py to Problemd10_v2.py

--- Problemd10_v2.py
+++ Problemd10_v2.py
@@ -1,4 +1,8 @@
 """
+Same as Problem10.py, just optimized it for multiple test cases
+"""
+
+"""
 When doing this question, I got to a big problem. There were 7 test cases in total. Out of 7, 5 of them were solved but 2 of them exceeded time limit. 
 I tried optmizing the code as much as I can but still 6 and 7 never solved. Then seeing the discussion section, I found out the mistake.
 
