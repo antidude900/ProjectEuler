cc7570a060b09c2f5d3d94daea69185389ab197a
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-09-25 19:42:44 +0545

    Update Problemd16.py

--- Problemd16.py
+++ Problemd16.py
@@ -1,9 +1,21 @@
-sum=0
+"""
+What is the sum of the digits of the number 2^^n?
+https://www.hackerrank.com/contests/projecteuler/challenges/euler016/problem
+"""
+def sum_of_pow_2(n):    
+    sum=0
+    
+    num=str(pow(2,n)) #for 2 to the power 1000
+                    #and changing into string so that we can iterate through it
 
-n=pow(2,1000) #for 2 to the power 1000
-
-num = str(n) #so that we can iterate throught the number
-
-for i in range(len(num)):
-	sum+=int(num[i]) #adding all the digits
-print(sum)
+    for i in range(len(num)):
+        sum+=int(num[i]) #adding all the digits
+    return sum
+	
+    
+if __name__ == "__main__":
+    t=int(input())
+    
+    for _ in range(t):
+        n = int(input())
+        print(sum_of_pow_2(n))
