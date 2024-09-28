f20053163c471f77822f5dbc2bdf9d37489c7c01
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-09-28 20:53:48 +0545

    Create Problemd22.py

---
+++ Problemd22.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    # for mapping alphabet with their position value [here position valus is given by index but as index starts from 0, need to add 1 to index value to get true positional value]
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+
+    t= int(input())
+    names  = sorted(input() for _ in range(t)) #sorting namesin alphabetical order as per question
+    
+    t = int(input()) #taking input for the Q queries
+    for i in range(t):
+        name = input()
+        total = sum(alphabet.index(x)+1 for x in name)  #adding positional value of each alphabet
+        total *=names.index(name)+1 #multiplying by the positional value of the given name in the list of names 
+        print(total)
+        
