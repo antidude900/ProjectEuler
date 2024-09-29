27cbe4690e61e0e83c2776b7d37c220f50984d0d
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-09-29 21:35:35 +0545

    Update Problem25.py

--- Problem25.py
+++ Problem25.py
@@ -1,17 +1,21 @@
-def compute_fibonacci_terms(max_n):
-    fib_digits = {}
+"""
+https://www.hackerrank.com/contests/projecteuler/challenges/euler025/problem
+"""
+
+def compute_fibonacci_terms(max_n): #computing all the nth terms of all cases together so that we dont have to start from first for all questions
+    fib_digits = {} 
     a, b = 1, 1
-    index = 2
+    term = 2 #we are at 2nd term
     
     current_digits = 1
     fib_digits[1] = 1
-    
+
     while current_digits <= max_n:
         a, b = b, a + b
-        index += 1
+        term += 1
         if len(str(b)) > current_digits:
             current_digits = len(str(b))
-            fib_digits[current_digits] = index
+            fib_digits[current_digits] = term
     
     return fib_digits
 
