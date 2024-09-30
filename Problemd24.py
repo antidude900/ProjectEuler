70910634972a4477ee4a98b1c00881334c055c3f
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-09-30 21:26:20 +0545

    Update Problemd24.py

--- Problemd24.py
+++ Problemd24.py
@@ -1,6 +1,7 @@
 """
 https://www.hackerrank.com/contests/projecteuler/challenges/euler024/problem
 """
+
 import math
 
 def nth_lexicographic_permutation(n):
@@ -14,20 +15,26 @@ def nth_lexicographic_permutation(n):
     """
     We have total 13 letters so 13! permutation in total. 
     Also, 13! = 13*12! meaning each of the 13 characters can make 12! permutation for itself(itself at first while others are doing permutation among them)
+    
     So while n<12!, 'a' is at first of the permutation and after n>12!, any other letter after 'a'comes at first. 
     for eg: if n=12!+1, 'b' comes at first of permutation as the permutations of a has ended
     Thus what comes at first can be determined by doing n/12! 
+    
     Now we remove 'a' and thus so we have 12 letters and thus 11! for each. 
+    
     By updating n' = n%12!, we will know how much up it was from 12! which will help us to find the first letter of the n' permutation of the remainaing string
     which is subsequently the second letter of the n! permutation.
     
     [Permutation of a number can be divided in these steps: (Lets take example of 4 digit number to find nth permutation)
     First, the permutation of the whole number is carried from which the 1st number for the nth permutation is picked 
-    and then the permutation is broken to the permutation between the remaning 3
+    and then the permutation is broken to the permutation between the remaning 3  
+    
     Second, the permutation of the remaning 3 is carried from which the 2nd number for the nth permutation is picked  
     and then the permutation is broken to the permutation between the remaning 2
+    
     Third, the permutation of the remaning 2 is carried from which the 3rd number for the nth permutation is picked  
     and then the permutation is broken to the permutation between the remaning 1
+    
     Finally, the remaning one is the last number of the nth permutation
     """
     
