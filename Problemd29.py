7199cc18af744b7442b1d38c66fb5ea2e3a1361f
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-10-17 21:58:02 +0545

    Update Problemd29.py

--- Problemd29.py
+++ Problemd29.py
@@ -3,5 +3,9 @@ https://www.hackerrank.com/contests/projecteuler/challenges/euler029/problem
 """
 
 """
+Tommorow will write about this 
+"""
+
+"""
 https://www.youtube.com/watch?v=e8VmPEdd954
 """
