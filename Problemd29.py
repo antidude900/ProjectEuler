db51787a10ba4130cdeb17919557137b6c24fef1
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-10-22 18:02:06 +0545

    Update Problemd29.py

--- Problemd29.py
+++ Problemd29.py
@@ -2,10 +2,30 @@
 https://www.hackerrank.com/contests/projecteuler/challenges/euler029/problem
 """
 
-"""
-Tommorow for sure will write about this, reallyyy
-"""
+def distinct_terms(N):
+    count = 0
+    tested = [0]*(N+1)
+    
+    for i in range(2, N+1):
+        if tested[i]: 
+            continue
+            
+        overlapped = set()
+        power = 2
+        
+        while (i**power <= N):
+            tested[i**power] = True
+            s = [j*power for j in range(2, N+1) if j*power > N]
 
-"""
-https://www.youtube.com/watch?v=e8VmPEdd954
-"""
+            overlapped.update(s)
+           
+            power += 1
+
+        count += len(overlapped) + N - 1
+
+    return count
+    
+if __name__ == "__main__":
+    N = int(input())      
+    print(distinct_terms(N))
+    
