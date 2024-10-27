3168e89bc359a176816a56dedbb0bf74fade3604
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-10-27 22:29:36 +0545

    Update Problemd36.py

--- Problemd36.py
+++ Problemd36.py
@@ -1,13 +1,22 @@
+
+"""
+For converting a natural number i.e base 10 to any other base say K, we have to divide the number by K until 0. 
+For every division, we record the remainder and the combined remainder from bottom to top is the number in base K.
+"""
+
 def convert(num,base):
-    if num==0:
-        return 0
+    if num == 0: #if num = 0, the while loop below wont even run thus returning a empty string. To avoid that, we manually return 0 
+        return "0"
         
     combined_remainder=''
+    
     while num>0: 
-        remainder=num%base
-        num=int(num/base)
+        remainder = num % base
+        num = int(num / base)
+        #could have directly done num,remainder = divmod(num,base)
        
-        combined_remainder=str(remainder)+combined_remainder
+        combined_remainder = str(remainder) + combined_remainder #as the later remainder should be at beginning, we add the previous remainder back of the current remainder
+        
     return combined_remainder    
     
     
@@ -19,6 +28,7 @@ if __name__ == "__main__":
 
     for num in range(1,N):    
         converted_num = convert(num,K)
-        if str(num) == str(num)[::-1] and converted_num == converted_num[::-1]: 
+        if str(num) == str(num)[::-1] and converted_num == converted_num[::-1]:
+            #for slicing we have [start:stop:step]. If done [::step], it means we go through the whole string.
             total_sum+=num
     print(total_sum)
