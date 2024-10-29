c8c51dceac2a984b4373e4af58e9af30a68bc17f
Sambal Shrestha <86548747+antidude900@users.noreply.github.com>
2024-10-29 14:28:06 +0545

    Update Problemd39.py

--- Problemd39.py
+++ Problemd39.py
@@ -15,7 +15,6 @@ and the perimeter is given by;
 p = a + b + c = 2 * m^2 + 2 * m * n = 2 * m * (m + n)
 """
 
-import math
 def gcd(a, b): #finding the greates common divisor to check if coprime or not
     """
     We use Euclid's division algorithm to find the. It states that for two positive integers a and b;
@@ -25,45 +24,50 @@ def gcd(a, b): #finding the greates common divisor to check if coprime or not
         a, b = b, a % b,
     return a
 
-perimeter_ways = [0] * (5*10**6 + 1) # we find all the perimeters upto N(p<=N) so that we dont have to recompute for each test cases
-                                     # perimeter ways stores the no of ways to make each perimeters and each index number is the perimeter itself
-
-for m in range(1, 1581): #as perimeter should be less or equal to 5000000, the 2m^2 term should be less than 5000000 meaning m<1581
-                         #this is just an apporximation assuming m >> n so neglecting n and finding limit of m. In reality, m value will be more less than 1581 if we consider n also 
-    for n in range(1, m): 
-        if (m + n) % 2 and gcd(m, n) == 1: #checking if of opposite parity and coprimer
-            p = 2 * m * (m + n) 
-            if p > 5000000: #though we found m value for p<=5000000, still adding up n might lead to p>5000000. so handling that edge case
-                continue
-            perimiter_ways[p] += 1
-            i = 2
-            while i * p <= 5000000: 
-            	"""
-               	the multiples of the perimeter p subsets the no of ways of p i.e if p is made from (a,b,c), 2*p will be made from(2*a,2*b,2*c)
-               	so increase the no of ways of 2p as well for the solution (2*a,2*b,2*c) when we find solution of p as (a,b,c). 
-               	Also because of the m and n nature of coprime and opposite polarity, it avoids genertaing m and n which generates same solution (2*a,2*b,2*c) for 2p
-               	Thus there is no risk of duplicates
-               	"""
-                primiter[i * p] += 1
-                i += 1
-
-max_perimeter_upto = [0] * (5*10**6 + 1) # this array stores the perimeter having the maximum no of ways among the perimeters upto given index number
-
-
-for perimeter in range(1, 5000 * 1000 + 1):
-  
-    max_perimeter = max_perimeter_upto[i-1] #We take the current perimeter with the maximum no of ways
-
-    # and then compare it to the the no of ways of the next perimeter
-    if perimeter_ways[perimeter] > perimeter_ways[max_perime]: #if that next perimeter has more no of ways than the current perimeter with max no of ways
-        max_perimeter_upto[perimeter] = perimeter #we update that the max no of ways upto that next perimeter is of the nextperimeter itself
-      
-    else: #but if that next perimeter has less no of ways than the current perimeter with max no of ways
-        max_perimeter_upto[perimeter] = max_perimeter #then the max no of ways upto that next perimeter is still of the previous one
-
+def max_ways_perimeter_upto_N(N):
+    perimeter_ways = [0] * (N + 1) # we find all the perimeters upto N(p<=N) so that we dont have to recompute for each test cases
+                                         # perimeter ways stores the no of ways to make each perimeters and each index number is the perimeter itself
+    
+    for m in range(1, int((N/2)**0.5)): #as perimeter should be less or equal to N, the 2m^2 term should be less than N meaning m<sqrt(N/2)
+                             #this is just an apporximation assuming m >> n so neglecting n and finding limit of m. In reality, m value will be more less than 1581 if we consider n also 
+        for n in range(1, m): 
+            if (m + n) % 2 and gcd(m, n) == 1: #checking if of opposite parity and coprimer
+                p = 2 * m * (m + n) 
+                if p > N: #though we found m value for p<=N, still adding up n might lead to p>N. so handling that edge case
+                    continue
+                perimeter_ways[p] += 1
+                i = 2
+                while i * p <= N: 
+                    """
+                       the multiples of the perimeter p subsets the no of ways of p i.e if p is made from (a,b,c), 2*p will be made from(2*a,2*b,2*c)
+                       so increase the no of ways of 2p as well for the solution (2*a,2*b,2*c) when we find solution of p as (a,b,c). 
+                       Also because of the m and n nature of coprime and opposite polarity, it avoids genertaing m and n which generates same solution (2*a,2*b,2*c) for 2p
+                       Thus there is no risk of duplicates
+                    """
+                    perimeter_ways[i * p] += 1
+                    i += 1
+    
+    max_perimeter_upto = [0] * (N+1) # this array stores the perimeter having the maximum no of ways among the perimeters upto given index number
+    
+    
+    for perimeter in range(1, N+1):
+    
+        max_perimeter = max_perimeter_upto[perimeter-1] #We take the current perimeter with the maximum no of ways
+    
+        # and then compare it to the the no of ways of the next perimeter
+        if perimeter_ways[perimeter] > perimeter_ways[max_perimeter]: #if that next perimeter has more no of ways than the current perimeter with max no of ways
+            max_perimeter_upto[perimeter] = perimeter #we update that the max no of ways upto that next perimeter is of the nextperimeter itself
+          
+        else: #but if that next perimeter has less no of ways than the current perimeter with max no of ways
+            max_perimeter_upto[perimeter] = max_perimeter #then the max no of ways upto that next perimeter is still of the previous one
+    
+    return max_perimeter_upto
+    
 if __name__ == "__main__":
     T = int(input())
-    for _ in range(T):
-        N = int(input())
-        print(max_perimeter_upto(N))
+    N = [int(input()) for _ in range(T)]
+    max_N  = max(N)
+    max_perimeter_upto = max_ways_perimeter_upto_N(max_N)
+
+    [print(max_perimeter_upto[n]) for n in N]
 
